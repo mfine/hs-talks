ce86e72359c44119518346a7afc2897e0e656ef1
Mark Fine <mark.fine@gmail.com>
2015-10-13 14:05:34 -0700

    v1

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,7 +2,32 @@ module Lib
   ( main'
   ) where
 
--- v0: The dreaded blank slate...
+-- v1 : Break the problem into two parts: parsing input (rankings)
+--      and calculating output (results).
+
+-- data ----------------------------------------------------
+
+-- Ranking information.
+data Ranking = Ranking
+  {
+  } deriving Show
+
+-- Result information.
+data Result = Result
+  {
+  } deriving Show
+
+-- parsing -------------------------------------------------
+
+-- Take a file and produce a list of team rankings
+parseRankings :: String -> IO [Ranking]
+parseRankings = undefined
+
+-- calculating ---------------------------------------------
+
+-- Take teams rankings and calculate results
+calculateResults :: [Ranking] -> [Result]
+calculateResults = undefined
 
 -- main ----------------------------------------------------
 
